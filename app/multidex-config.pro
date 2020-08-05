ee4cd5f2eca834eba4dcab44c7fa7813d4a670b4
Nate Ebel <nate@premise.com>
2020-08-05 13:05:12 -0700

    Initial commit

---
+++ app/multidex-config.pro
@@ -0,0 +1,1 @@
+-keep class com.premise.fullstorysandbox.FullStorySandboxApplication { *; }
---
+++ app/src/main/java/com/premise/fullstorysandbox/FullStorySandboxApplication.kt
@@ -0,0 +1,6 @@
+package com.premise.fullstorysandbox
+
+import androidx.multidex.MultiDexApplication
+
+class FullStorySandboxApplication : MultiDexApplication() {
+}
